056affcb6f1d3048e30b84284775668c3bc7693c
tadavarthi-rishi <rtadavar@asu.edu>
2024-04-21 17:43:16 -0700

    final commit added array of objects to add students

--- emailapp/src/studentDatabaseApp/StudentDatabase.java
+++ emailapp/src/studentDatabaseApp/StudentDatabase.java
@@ -1,18 +1,31 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class StudentDatabase {
 
 	public static void main(String[] args) {
-		Student st1 = new Student();
-		st1.enroll();
-		st1.paytuition();
-		System.out.println(st1.tostring());
-		// TODO Auto-generated method stub
+		
+
 		
 		//Ask how many users want to add
+		System.out.println("Enter the number of students to enroll:");
+		Scanner in = new Scanner(System.in);
+		int numofstudents = in.nextInt();
+		Student[] students = new Student[numofstudents];
 		
+		
+
 		//create n number of new students
 		
+		
+		for(int n=0;n<numofstudents;n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].paytuition();
+			System.out.println(students[n].tostring());
+			
+		}
 		
 		
 		
